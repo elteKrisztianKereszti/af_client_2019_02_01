a5e3036747fb8113dc19cf41aded05f9392311ce
kereszti.krisztian <kereszti.krisztian@marktsoft.hu>
2020-05-08 18:17:09 +0200

    user entity and aut service skeleton

---
+++ src/app/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { User } from './user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  public isLoggedIn = false;
+  public user: User;
+  public redirectUrl: string;
+
+  constructor() {
+
+  }
+
+  public login(username: string, password: string): void {
+
+  }
+
+  public logout(): void {
+
+  }
+}
